cf1800f6552c99a8a52f3833e96c175075fe8ef1
Gabriel Fuentes <45942967+gabrielfuenmar@users.noreply.github.com>
2021-02-12 23:43:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ def login():
     formlog=LoginRecord(request.form)
     
     if request.method =="POST" and formlog.validate_on_submit():
-        ##check user
+        ##check user. Add descendent to have record specific for the groupo first O.W admin record first
         user=SuperUser.query.filter_by(user_name=formlog.user.data).order_by(SuperUser.id.desc()).first()
         
         if user and formlog.password.data == user.password and GROUP_NAME==user.group_name:
